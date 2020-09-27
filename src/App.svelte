2407fefe1b00c84739180c0c1ef2928092b48a40
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2020-09-27 03:02:58 +0200

    Perlin noise

--- src/App.svelte
+++ src/App.svelte
@@ -1,38 +1,7 @@
 <script>
-let xsize = 40;
-let ysize = 40;
-let points = [];
-
-for(let x=0; x<xsize; x++) {
-  for(let y=0; y<ysize; y++) {
-    let c = Math.random() > 0.5 ? 'red' : 'blue';
-    points.push({x,y,c});
-  }
-}
+  import Uniform from "./Uniform.svelte";
+  import Perlin from "./Perlin.svelte";
 </script>
 
-<style>
-header {
-  font-size: 400%;
-  text-align: center;
-}
-.map {
-  background-color: #ccc;
-  height: 800px;
-  width: 800px;
-  margin: auto;
-  display: block;
-}
-rect {
-  stroke-width: 1px;
-  stroke: black;
-}
-</style>
-
-<header>Uniform Noise</header>
-
-<svg class="map">
-  {#each points as {x,y,c}}
-    <rect x={20*x} y={20*y} height=20 width=20 style={`fill:${c}`} />
-  {/each}
-</svg>
+<!-- <Uniform /> -->
+<Perlin />
