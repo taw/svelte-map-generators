38bfb730dee0766e33b32a40d541e24b396bb895
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2020-09-27 03:41:45 +0200

    Make it an island surrounded by water

--- src/Perlin.svelte
+++ src/Perlin.svelte
@@ -1,8 +1,8 @@
 <script>
   import noise from "./perlin";
 
-  let xsize = 40;
-  let ysize = 40;
+  let xsize = 80;
+  let ysize = 80;
   let points = [];
 
   let jx = Math.random();
@@ -20,17 +20,23 @@
       let n =    noise((x + jx)/4, (y + jy)/4) +
           0.5 *  noise((x + jx)/8, (y + jy)/8) +
           0.25 * noise((x + jx)/16, (y + jy)/16) +
-          0.125 * noise((x + jx)/32, (y + jy)/32);
-      let z = 1 + 0.5 + 0.25 + 0.125;
+          0.125 * noise((x + jx)/32, (y + jy)/32) +
+          0.125 * noise((x + jx)/64, (y + jy)/64);
+      let z = 1 + 0.5 + 0.25 + 0.125 + 0.125;
+      // go way down on edges
+      let cx = Math.abs(2*x/(xsize-1) - 1)
+      let cy = Math.abs(2*y/(ysize-1) - 1)
+      n -= Math.pow(cx, 5) + Math.pow(cy, 5)
+
       n = clamp01(((n / z) + 1) / 2);
       let c;
-      if (n < 0.48) {
+      if (n < 0.45) {
         c = 'blue;'
-      } else if (n < 0.55) {
+      } else if (n < 0.50) {
         c = 'green';
-      } else if (n < 0.60) {
+      } else if (n < 0.55) {
         c = 'yellow';
-      } else if (n < 0.65) {
+      } else if (n < 0.60) {
         c = 'orange';
       } else {
         c = 'red';
@@ -64,6 +70,6 @@
 
   <svg class="map">
     {#each points as {x,y,c}}
-      <rect x={20*x} y={20*y} height=20 width=20 style={`fill:${c}`} />
+      <rect x={10*x} y={10*y} height=10 width=10 style={`fill:${c}`} />
     {/each}
   </svg>
